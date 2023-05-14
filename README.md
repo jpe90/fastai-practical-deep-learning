5dec80ac320fe5f3c1486295ab1dd22948dd76e9
Jon Eskin <eskinjp@gmail.com>
2023-05-14 04:19:33 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+fastai's [practical deep learning for coders](https://course.fast.ai/) displays course lessons as jupyter notebooks
+
+this repository aims to provide the course material as python scripts you can run on your system
---
+++ setup.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# setup and activate a venv for all files in this tutorial
+python3 -m venv venv
+source venv/bin/activate
+
+# dependencies for part 1 lesson 1
+pip install -Uqq fastai
+pip install -Uqq duckduckgo_search
